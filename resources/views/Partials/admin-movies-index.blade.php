5bf0f9c0b8edbc3be1f48a2bd2c26812daff18dd
AndreaRaga56 <andrearaga56@gmail.com>
2025-06-11 15:58:31 +0200

    feat index style

--- resources/views/Partials/admin-movies-index.blade.php
+++ resources/views/Partials/admin-movies-index.blade.php
@@ -7,44 +7,45 @@
         <div class="container d-flex justify-content-center flex-column align-items-center">
             <h1 class="my-4 titolo">Tutti i Film</h1>
 
-            <div class=" btn-index mb-3">
-                <a href="{{ route('movies.create') }}" class="btn btn-secondary m-1">Aggiungi un nuovo Film</a>
+            <div class=" btn-index mb-2">
+                <a href="{{ route('movies.create') }}" class="btn btn-secondary my-1">Aggiungi un nuovo Film</a>
                 <a href="
             {{-- {{ route('genres.index') }} --}}
-            " class="btn btn-secondary m-1">Gestisci Generi</a>
+            " class="btn btn-secondary my-1">Gestisci Generi</a>
                 <a href="
             {{-- {{ route('streaming-platforms.index') }} --}}
-            " class="btn btn-secondary m-1">Gestisci
+            " class="btn btn-secondary my-1">Gestisci
                     Piattaforme</a>
             </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Titolo</th>
-                        <th>Anno</th>
-                        <th class="regista">Regista</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($movies as $movie)
-                        <tr>
-                            <td>{{ $movie->title }}</td>
-                            <td>{{ $movie->release_year }}</td>
-                            <td class="regista">{{ $movie->director }}</td>
-                            <td class="d-flex justify-content-center action">
-                                <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Visualizza</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-        </div>
-    </main>
-
-
-
-
-
-@endsection
+            <div class="my-table">
+                <div class="row justify-content-center align-items-center">
+                    <div class="th col-6 col-md-4 py-2">Titolo</div>
+                    <div class="th col-2 col-md-1 py-2">Anno</div>
+                    <div class="th col-4 py-2 regista">Regista</div>
+                    <div class="th col-4 col-md-3 py-2"></div>
+                </div>
+                <hr />
+                @foreach ($movies as $movie)
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-6 col-md-4 py-2">
+                            {{ $movie->title }}
+                        </div>
+                        <div class="col-2 col-md-1 py-2">
+                            {{ $movie->release_year }}
+                        </div>
+                        <div class="col-4 py-2 regista">
+                            {{ $movie->director }}
+                        </div>
+                        <div class="col-4 col-md-3 py-2">
+                            <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Visualizza</a>
+                        </div>
+                    </div>
+                    @if (!$loop->last)
+                        <hr />
+                    @endif
+                @endforeach
+            </div>
+            </div>
+            </main>
+        @endsection
